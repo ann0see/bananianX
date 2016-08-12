69c3ede0cc62daf5c0476ae13e03b787ac0ff26d
jonisc <jonathansmithchung@t-online.de>
2016-08-12 20:22:32 +0200

    Update bananianX.sh

--- bananianX.sh
+++ bananianX.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Install bananianX
-echo "Welcome. It might take some time..."
+echo "Welcome. It might take some time... If your internet connection is too slow it will take more time. Your Pi will reboot if this script is finished. You can abort it with the command which is shown then."
+sleep 1
 echo "Updating..."
 apt-get update
 echo "Installing lightdm..."
@@ -16,7 +17,7 @@ apt-get -qq install openbox
 echo "Installing sudo, if you want to add an other user with root posibility..."
 apt-get -qq install sudo
 echo "Setting up X..."
-echo "setting up X, so that a normal user can shutdown..."
+echo "Setting up X, so that a normal user can shutdown..."
 apt-get -qq install systemd-shim
 echo "session required pam_systemd.so" >> /etc/pam.d/lxdm
 clear
