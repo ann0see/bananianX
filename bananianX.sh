caf08af8a91425e7c06e2e2bb2a6a5638396bafd
jonisc <jonathansmithchung@t-online.de>
2016-08-12 20:05:25 +0200

    Update bananianX.sh

--- bananianX.sh
+++ bananianX.sh
@@ -6,15 +6,17 @@ apt-get update
 echo "Installing lightdm..."
 apt-get -qq install lightdm
 apt-get -qq install lightdm-gtk-greeter
-echo "installing LXDE Core"
+echo "Installing LXDE Core..."
 apt-get -qq install lxde-core
-echo "OK, install xinit"
+echo "OK, install xinit..."
+echo "Xinit let's you start the X-Server with startx..."
 apt-get -qq install xinit
-echo "Installing openbox Window Manager"
+echo "Installing openbox Window Manager..."
 apt-get -qq install openbox
-echo "Installing sudo, if you want to add an other user, not root"
+echo "Installing sudo, if you want to add an other user with root posibility..."
 apt-get -qq install sudo
-echo "Setting up X"
+echo "Setting up X..."
+echo "setting up X, so that a normal user can shutdown..."
 apt-get -qq install systemd-shim
 echo "session required pam_systemd.so" >> /etc/pam.d/lxdm
 clear
