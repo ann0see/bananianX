7aecfdc8c31697c2a3f45be08bc7f95199e36666
Jonathan Smith-Chung <bananianx@t-online.de>
2016-08-20 13:05:16 +0200

    Edited

--- bananianX.sh
+++ bananianX.sh
@@ -14,7 +14,7 @@ CHOICE_HEIGHT=4
 BACKTITLE="BananianX installer"
 TITLE="Install BananianX"
 MESSAGETITLE="Message"
-MESSAGETEXT="Hi. This script will install a GUI (LIGHTDM, LXDE, Openbox) on your Banana Pi. Please wait and don go away. You will be able to choose which version of BananianX you want to install. It might take some time... Your Pi will reboot 10 min. after this script is finished. You can abort the reboot."
+MESSAGETEXT="Hi. This script will install a GUI (LIGHTDM, LXDE, Openbox) on your Banana Pi. Please wait and don't leave the Pi alone for now. You will be able to choose which version of BananianX you want to install. It might take some time... After the script is finished, the Pi will restart within 10 minutes. You can abort the reboot."
 MENU="Choose one of the following options:"
 OPTIONS=(1 "Option 1: BananianX LIGHT (only packets you really need-> less features, takes not as long as Option 2 to install, uses less disk space)"
          2 "Option 2: BananianX FULL (everything-> more features, but takes longer to install, uses more disk space) (recommended)"
@@ -24,7 +24,7 @@ INSTALLDOGGY="Install 'Doggy' ;-)..."
 INTRODOGGY="Hi! I'm Doggy. I'll install the GUI for you. OK. Let's start..."
 UPDATEMSG="Let me update..."
 
-LETALONE="Oh, now you can leave me alone. I'll restart the Pi 10 Minutes after the script is finished."
+LETALONE="Oh, now you can leave me alone. I'll restart the Pi 10 minutes after the script is finished."
 
 INST_LIGHTDM="Installing lightdm..."
 
@@ -32,9 +32,9 @@ INST_LXDE="Installing LXDE Core..."
 
 INST_XINIT="OK, install xinit..."
 
-DES_XINIT="Xinit lets you start the X-Server with 'startx'..."
+DES_XINIT="Xinit allows you to start the X-Server with 'startx'..."
 
-INST_OPENBOX="Installing openbox window Manager..."
+INST_OPENBOX="Installing openbox window manager..."
 
 SETUP_X="Setting up X..."
 
