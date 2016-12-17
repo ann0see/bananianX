a976d3995c1a19bdf2270d3e65162c319e5791c1
jonisc <bananianx@t-online.de>
2016-12-17 13:37:40 +0100

    Gksu

--- bananianX.sh
+++ bananianX.sh
@@ -50,7 +50,7 @@ NO_RESTART="OK. I won't restart :-("
 PLS_RESTART="But please restart the Pi with command (reboot) later."
 # Declare install variables; please do not change if you don't know what that is
 # Optional packages:
-OPTPACK_APT="sudo gparted xrdp mc iceweasel pcmanfm avahi-daemon xarchiver galculator gksudo"
+OPTPACK_APT="sudo gparted xrdp mc iceweasel pcmanfm avahi-daemon xarchiver galculator gksu"
 #If you don't know what you are doing, don't edit after this line!
 ######################################
 
